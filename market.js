87f140b60c5ed74d714398ab653919fcaba84b78
AndriyDidkivskij <158279895+AndriyDidkivskij@users.noreply.github.com>
2024-05-12 20:21:33 +0300

    м

--- market.js
+++ market.js
@@ -1,24 +1,83 @@
+// // Перевірка підключеного файлу скриптів
+// console.log('Перевірка підключеного файлу скриптів market.js')
 
-//перевірка підключеного файлу скриптів
-console.log('перевірка підключеного файлу скриптів')
+//Визначення масиву товарів
+let itemsArray = [
+    'Газонокосарка 43',
+    'Електричний тример 110',
+    'Електрична газонокосарка 32',
+    'Акумуляторний оприскувач 12 N',
+    // 'Газонокосарка 430',
+    // 'Електричний тример 130',
+    // 'Електрична газонокосарка 320',
+    // 'Акумуляторний оприскувач 12 E',
+]
 
-//отримання елементу з ідентифікатаром Items
-let itemsDiv = document.getElementById("items")
+// Отримання елементу з ідентифікатором items
+let itemsDiv = document.getElementById("items");
 
-if (itemsDiv){
-    //Вивід знайденого елементу
-console.log(itemsDiv)
+// Перевірка існування знайденого блоку
+if (itemsDiv) {
+    // Вивід знайденого елементу
+    // console.log(itemsDiv)
 
-//Додвання відформатованого HTML коду в блок
-//itemsDiv.innerHTML += '<div class = item></div>'
-//itemsDiv.innerHTML += '<div class = item></div>'
-//itemsDiv.innerHTML += '<div class = item></div>'
-//itemsDiv.innerHTML += '<div class = item></div>'
-    for (let i=0; i<100; i++){
-        itemsDiv.innerHTML += '<div class = "item"></div'
-    }
+    // // Вивід значення поля знайденого елементу
+    // console.log('Поле classList: ', itemsDiv.classList)
+    // console.log('Поле id: ', itemsDiv.id)
+    // console.log('Поле clientWidth: ', itemsDiv.clientWidth)
+    // console.log('Поле innerHTML: ', itemsDiv.innerHTML)
+
+    // // Додавання тексту в блок
+    // itemsDiv.innerText = 'Перший Програмно доданий текст'
+    // // itemsDiv.innerText = 'Другий Програмно доданий текст' 
+    // itemsDiv.innerText += 'Другий Програмно доданий текст'
+
+    // Додавання відформатованого HTML коду в блок
+    // itemsDiv.innerHTML = '<div class = item></div>'
+    // itemsDiv.innerHTML += '<div class = item></div>'
+    // itemsDiv.innerHTML += '<div class = item></div>'
+    // itemsDiv.innerHTML += '<div class = item></div>'
+    // itemsDiv.innerHTML += '<div class = item></div>'
+
+    // for (let i = 0; i < 100; i++) {
+    //     itemsDiv.innerHTML += '<div class = item></div>'
+    // }
+    //Виведення елементів масиву
+    itemsArray.forEach((item, index) => {
+        // console.log(item)
+        // itemsDiv.innerText += item
+        // itemsDiv.innerHTML += `<div class = "item">${item}</div>`
+        //Виводимо на веб сторінку елемент масиву в блок з класом item
+    
+    })
 } else {
-//вивід повідомлення про не знайдений блок
-console.log('Блок товарів не знайдено')
+    //Вивід повідомлення про не знайдений блок
+    console.log('Блок товарів не знайдено')
 }
 
+
+// //Виведення елементів масиву
+// itemsArray.forEach((item) => {
+//     console.log(item)
+// })
+
+// //Виведення елементів відсортованого масиву
+// itemsArray.sort().forEach((item) => {
+//     console.log(index + '-й елемент', item)
+// })
+
+// //Виведення в консоль масиву
+// console.log(itemsArray)
+
+// //Виведення в консоль елементів масиву
+// for (let i=0; i<itemsArray.length; i++) {
+//     console.log(itemsArray[i])
+// }
+
+// //Сортування масиву
+// itemsArray = itemsArray.sort()
+
+// //Виведення в консоль номерів та значень елементів масиву
+// for (let i=0; i<itemsArray.length; i++) {
+//     console.log(i + '-й елемент: ', itemsArray[i])
+// }
